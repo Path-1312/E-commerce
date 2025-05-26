76a918fe5d039d4ce214d51a355f4d1ff13574b2
Path Bhimani <pathbhimani1312@gmail.com>
2025-05-26 15:47:48 +0530

    updated about page

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 import datetime
 from django.core.validators import MinValueValidator
+import os
 
 class Product(models.Model):
     product_name = models.CharField(max_length=50, default='')
     category = models.CharField(max_length=50, default='')
     sub_category = models.CharField(max_length=50, default='')
-    price = models.IntegerField(default=0)
+    price = models.IntegerField(validators=[MinValueValidator(0)])
     number_of_stock = models.IntegerField(validators=[MinValueValidator(0)])
     description = models.CharField(max_length=200, default='')
     published_date = models.DateField(default=datetime.date.today)
@@ -14,6 +15,11 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product_name
+    
+    def delete(self, *args, **kwargs):
+        if self.image and os.path.isfile(self.image.path):
+            os.remove(self.image.path)
+        super().delete(*args, **kwargs)     
 
 
 
