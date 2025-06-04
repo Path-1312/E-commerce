73cc393f9021cda6cfbf8061b5bddb6bbc700af4
Path Bhimani <pathbhimani1312@gmail.com>
2025-06-04 19:02:39 +0530

    updated cart

--- blog/views.py
+++ blog/views.py
@@ -85,12 +85,13 @@ def checkout(request):
 def view_cart(request):
     cart_items = CartItem.objects.filter(user=request.user)
     total_price = sum(item.product.price * item.quantity for item in cart_items)
-    return render(request, 'blog/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+    p = {'cart_items': cart_items, 'total_price': total_price}
+    c = {**p, **params}
+    return render(request, 'blog/cart.html', c)
 
 def add_to_cart(request, item_id):
     product = Product.objects.get(id=item_id)
-    cart_item, created = CartItem.objects.get_or_create(product=product, 
-                                                       user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(product=product, user=request.user)
     cart_item.quantity += 1
     cart_item.save()
     return redirect('cart:view_cart')
